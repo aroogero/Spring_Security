83bd523322457a94f03485a9a5a2cf3983e67ccd
zh_bexultan <you@example.com>
2023-03-17 18:47:11 +0600

    Spring. SECURITY 2. Les - 20(video) St - 8. Написали метод сервиса в сервисе, который находит и вытаскивает пользователя

--- src/main/java/kz/bitlab/springsecurity/bootcamp4security/service/UserService.java
+++ src/main/java/kz/bitlab/springsecurity/bootcamp4security/service/UserService.java
@@ -15,13 +15,17 @@ public class UserService implements UserDetailsService {
     //у него есть абстрактный метод loadUserByUsername(String username) - подтянуть юзера по его уникальной юзернейм
     //и наследуя от него, нам надо реализовать его метод
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepository ;
 
     @Override  //переписывание
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException { //это метод сервиса, не репозитори
         //нам тут нужно возвращать пользователя по имейлу. В UserRepository у нас нет метода, который возвращает пользователя по имейлу
-        User user = userRepository.findByEmail(username);
-        if (user == null) throw new UsernameNotFoundException("User not found");
-        return user;
+        User user= userRepository.findByEmail(username);
+        if(user == null) throw new UsernameNotFoundException("User not found");
+        return user ;
     }
+    //Мы переписали метод секьюрити, который ищет пользователя под себя. Вход и выход мы переписали
+    //А тут говорим, чувак, ищи пользователя по моим критериям
+    //Если не писать, то секьюрити по своему будет искать пользователя, и ты не найдешь пользователя
+    //я сам буду тебе передавать кого искать. Если ты найдешь туда пойдешь, если не найдешь сюда
 }
