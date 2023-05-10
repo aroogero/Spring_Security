626aef224b147d76d0d9ea18b61930c65cddea29
zh_bexultan <you@example.com>
2023-05-10 14:42:33 +0600

    Spring.REST 2.Les - 23(video) St-10. Прописали рест контроллер который вытаскивает все данные

---
+++ src/main/java/kz/bitlab/springsecurity/bootcamp4security/api/PostController.java
@@ -0,0 +1,24 @@
+package kz.bitlab.springsecurity.bootcamp4security.api;
+
+import kz.bitlab.springsecurity.bootcamp4security.model.Post;
+import kz.bitlab.springsecurity.bootcamp4security.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController //то же, что и контроллер, но возвращаемый тип данных не будет HTML-ом
+//Для передачи данных мобильной разработке, мы должны отправлять данные в чистом виде, без фронта
+//Поэтому передаем в виде JSON
+@RequestMapping(value = "/api/post") //это его префикс
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+    @GetMapping//у него нет никакой ссылки - это означает что ссылка по умолчанию будет вытаскивать все данные - /api/post
+    public List<Post> getPosts() {
+        return postService.getPosts();
+    }
+}
